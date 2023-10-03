8090ed0f268158dfb7d793d743bc6ff04e67700b
Pavel <malinatrashh@gmail.com>
2023-10-03 17:54:59 +0800

    create welcome screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import Favicon from '@assets/favicon.png';
+
 const Home = () => {
-	return <div>Home</div>;
+	const wrapper = 'flex h-screen justify-center items-center';
+	const content = 'flex items-center text-[2rem] gap-[5rem]';
+	return (
+		<div className={wrapper}>
+			<div className={content}>
+				<div>
+					<h1>malinatrash</h1>
+					<h1>React</h1>
+					<h1>Tailwind</h1>
+					<h1>Template</h1>
+				</div>
+				<img src={Favicon} />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
