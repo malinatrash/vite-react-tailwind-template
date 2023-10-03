826ffab7ea5f6be5bd6e73d1be00c8d6aa1051fb
Pavel <malinatrashh@gmail.com>
2023-10-04 00:14:49 +0800

    сделал очень крутой шаблон ставим лайки и подписываемся

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,24 @@
-import Favicon from '@assets/favicon.png';
+import Favicon from '@assets/logo.png';
 
 const Home = () => {
-	const wrapper = 'flex h-screen justify-center items-center';
-	const content = 'flex items-center text-[2rem] gap-[5rem]';
+	const wrapper =
+		'flex h-screen justify-center items-center bg-background select-none';
+	const content = 'flex items-center mobile:flex-col-reverse';
 	return (
 		<div className={wrapper}>
 			<div className={content}>
-				<div>
-					<h1>malinatrash</h1>
-					<h1>React</h1>
-					<h1>Tailwind</h1>
-					<h1>Template</h1>
+				<div className='flex items-center flex-col'>
+					<h1 className='hover:scale-150 transition-all duration-500 text-[4rem] mobile:text-[3rem]'>
+						malinatrash
+					</h1>
+					<h1 className='text-[3rem] mobile:text-[2rem]'>
+						React Tailwind Template
+					</h1>
 				</div>
-				<img src={Favicon} />
+				<img
+					className='w-96 transition-all duration-500 hover:scale-150'
+					src={Favicon}
+				/>
 			</div>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		screens: {
+			mobile: { max: '820px' },
+			xs: { max: '640px' },
+			xxs: { max: '370px' },
+		},
+		colors: {
+			background: '#E388AB',
+		},
 		extend: {},
 	},
 	plugins: [],
